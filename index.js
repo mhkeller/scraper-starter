58bebb89d7a98a9a4f2c09ae7a40212c40b92570
mhkeller <code@mhkeller.com>
2023-09-05 18:17:09 -0400

    update browser context

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
-import { launchBrowser } from '@mhkeller/utils/scrape';
 import { chromium } from 'playwright-extra';
 import stealth from 'puppeteer-extra-plugin-stealth';
 
 import authenticate from './lib/authenticate.js';
+import launchBrowser from './lib/launchBrowser.js';
 
-import { targetUrl } from '../config/config.js';
+import { targetUrl } from './config/config.js';
 
 chromium.use(stealth());
 
-const { browser, page } = await launchBrowser({ headless: false });
+const { browserContext, page } = await launchBrowser({ headless: false });
 
-await authenticate(browser, page, targetUrl);
+await authenticate(browserContext, page, targetUrl);
 
 /* --------------------------------------------
-	* Click into the audience input element, which will bring up the map
+	* Type a name into the discovery search bar
 	*/
-const locationInputSelector = 'div[data-testid="ads-targeting-location-typeahead"]';
-await page.waitFor(locationInputSelector);
-await page.click(locationInputSelector);
+const searchBar = await page.locator('input[name="search"]')
 
-// const mySelector = '.tk';
-// await page.waitForSelector(mySelector);
-
-// // Extract the results from the page.
-// const texts = await page.evaluate(sel => {
-// 	const things = Array.from(document.querySelectorAll(sel));
-// 	return things.map(thing => {
-// 		const text = thing.textContent.trim();
-// 		return text;
-// 	});
-// }, mySelector);
-
-// console.log(texts);
-
-await browser.close();
+await searchBar.type('testing', { delay: 100 });
+// await browser.close();
